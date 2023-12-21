257c44c4f8d452b47136b3f33059e7ea12fca6f2
minsangOh <flos9537@gmail.com>
2023-12-21 18:38:50 +0900

    플러스 마이너스 기능 추가

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h3 data-testid="counter">{counter}</h3>
-        <button data-testid="minus-button">-</button>
-        <button data-testid="plus-button">+</button>
+        <button data-testid="minus-button"onClick={()=> setCounter(prev => prev -1)}> -</button>
+
+        {/* prev 의 경우 이전 상태 값에 대한 참조로, 현재 카운터 값 */}
+        <button data-testid="plus-button" onClick={()=> setCounter(prev => prev +1)}>+</button>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,71 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
+// 카운터의 초기 값이 0인지 테스트하는 코드
 test("the counter starts at 0", () => {
   render(<App />);
+
+  // 'counter'라는 test ID를 가진 요소를 찾음
   const counterElement = screen.getByTestId("counter");
+
+  // 해당 요소의 텍스트 내용이 0인지 확인
   expect(counterElement).toHaveTextContent(0);
 });
 
+
+// 마이너스 버튼의 텍스트가 "-"인지 테스트하는 코드
 test("minus button has correct text", () => {
   render(<App />);
+  // 'minus-button'이라는 test ID를 가진 요소를 찾음
   const buttonElement = screen.getByTestId("minus-button");
+
+  // 해당 버튼의 텍스트 내용이 "-"인지 확인
   expect(buttonElement).toHaveTextContent("-");
 });
 
+
+// 플러스 버튼의 텍스트가 "+"인지 테스트하는 코드
 test("plus button has correct text", () => {
   render(<App />);
+  // 'plus-button'이라는 test ID를 가진 요소를 찾음
   const buttonElement = screen.getByTestId("plus-button");
+
+  // 해당 버튼의 텍스트 내용이 "+"인지 확인
   expect(buttonElement).toHaveTextContent("+");
+});
+
+
+// 플러스 버튼을 클릭했을 때 카운터가 1로 변경되는지 테스트하는 코드
+test("When the + button is pressed, the counter changes to 1", () => {
+  render(<App />);
+
+  // 'plus-button'이라는 test ID를 가진 요소를 찾음
+  const buttonElement = screen.getByTestId("plus-button");
+
+  // 찾은 버튼에 클릭 이벤트를 발생시킴
+  fireEvent.click(buttonElement);
+
+  // 'counter'라는 test ID를 가진 요소를 다시 찾음
+  const counterElement = screen.getByTestId("counter");
+
+  // 해당 요소의 텍스트 내용이 1인지 확인
+  expect(counterElement).toHaveTextContent(1);
+});
+
+
+// 플러스 버튼을 클릭했을 때 카운터가 -1로 변경되는지 테스트하는 코드
+test("When the - button is pressed, the counter changes to 1", () => {
+  render(<App />);
+
+  // 'plus-button'이라는 test ID를 가진 요소를 찾음
+  const buttonElement = screen.getByTestId("minus-button");
+
+  // 찾은 버튼에 클릭 이벤트를 발생시킴
+  fireEvent.click(buttonElement);
+
+  // 'counter'라는 test ID를 가진 요소를 다시 찾음
+  const counterElement = screen.getByTestId("counter");
+  
+  // 해당 요소의 텍스트 내용이 1인지 확인
+  expect(counterElement).toHaveTextContent(-1);
 });
