6ba8ed0f808771851f38af0c0c1b3ffaae9e0b85
minsangOh <flos9537@gmail.com>
2023-12-22 15:53:10 +0900

    on off 클릭시 버튼 활성화 비활성화

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
         <h3 data-testid="counter">{counter}</h3>
-        <button data-testid="minus-button"onClick={()=> setCounter(prev => prev -1)}> -</button>
+        <button
+          data-testid="minus-button"
+          onClick={() => setCounter((prev) => prev - 1)}
+          disabled={disabled}
+        >
+          -
+        </button>
 
         {/* prev 의 경우 이전 상태 값에 대한 참조로, 현재 카운터 값 */}
-        <button data-testid="plus-button" onClick={()=> setCounter(prev => prev +1)}>+</button>
-      
         <button
-        style={{backgroundColor:"blue"}}
+          data-testid="plus-button"
+          onClick={() => setCounter((prev) => prev + 1)}
+          disabled={disabled}
+        >
+          +
+        </button>
+
+        <button 
+        style={{ backgroundColor: "blue" }} 
         data-testid="on/off-button"
-        >on/off</button>
+        onClick={() => {setDisabled(prev => !prev)}}
+        >
+          on/off
+        </button>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -12,6 +12,8 @@ test("the counter starts at 0", () => {
   expect(counterElement).toHaveTextContent(0);
 });
 
+
+
 // 마이너스 버튼의 텍스트가 "-"인지 테스트하는 코드
 test("minus button has correct text", () => {
   render(<App />);
@@ -22,6 +24,8 @@ test("minus button has correct text", () => {
   expect(buttonElement).toHaveTextContent("-");
 });
 
+
+
 // 플러스 버튼의 텍스트가 "+"인지 테스트하는 코드
 test("plus button has correct text", () => {
   render(<App />);
@@ -31,6 +35,8 @@ test("plus button has correct text", () => {
   // 해당 버튼의 텍스트 내용이 "+"인지 확인
   expect(buttonElement).toHaveTextContent("+");
 });
+
+
 
 // 플러스 버튼을 클릭했을 때 카운터가 1로 변경되는지 테스트하는 코드
 test("When the + button is pressed, the counter changes to 1", () => {
@@ -49,6 +55,8 @@ test("When the + button is pressed, the counter changes to 1", () => {
   expect(counterElement).toHaveTextContent(1);
 });
 
+
+
 // 플러스 버튼을 클릭했을 때 카운터가 -1로 변경되는지 테스트하는 코드
 test("When the - button is pressed, the counter changes to 1", () => {
   render(<App />);
@@ -66,9 +74,34 @@ test("When the - button is pressed, the counter changes to 1", () => {
   expect(counterElement).toHaveTextContent(-1);
 });
 
-test("on/off button has bluew color", () => {
+
+
+// on/off 버튼이 파란색인지 테스트하는 코드
+test("on/off button has blue color", () => {
   render(<App />);
 
+  // 'on/off-button'이라는 test ID를 가진 요소를 찾음
   const buttonElement = screen.getByTestId("on/off-button");
+
+  // 해당 버튼의 배경색이 파란색인지 확인
   expect(buttonElement).toHaveStyle({ backgroundColor: "blue" });
+});
+
+
+
+// on/off 버튼 클릭 시 -, = 버튼이 비활성화되는지 테스트하는 코드
+test("prevent the -, = button from being pressed when the on/off button is clicked", () => {
+  render(<App />);
+
+  // 'on/off-button'이라는 test ID를 가진 요소를 찾음
+  const onOffButtonElement = screen.getByTestId("on/off-button");
+
+  // 찾은 on/off 버튼에 클릭 이벤트를 발생시킴
+  fireEvent.click(onOffButtonElement);
+
+  // 'plus-button'이라는 test ID를 가진 요소를 찾음
+  const plusButtonElement = screen.getByTestId("plus-button");
+
+  // 해당 버튼이 비활성화 상태인지 확인
+  expect(plusButtonElement).toBeDisabled();
 });
